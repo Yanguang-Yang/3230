ca5031b70e42a8c97da25fc028744209215048d4
Yanguang-Yang <yyg522743064@gmail.com>
2022-03-15 02:57:58 +0800

    lab5_1

--- lab05/scripts/index.js
+++ lab05/scripts/index.js
@@ -1,32 +1,27 @@
 $(document).ready(function(){
 
-    let row = $('tr');
+    let row = $('.row');
     row.click(selectRow)
 
     let col = $('tr:first-child th')
     col.click(selectColumn)
 
-    let one = $('tr:first-child th:first-child')
-    one.click(del)
-
     function selectColumn(){
+        del()
         let index = $(this).index()
-        //alert(index)
         let s = $('tr td:nth-child(' + (index+1) + ')')
-        //$('tr:nth-child(2) td').addClass('selected')
         s.addClass('selected')
     }
 
     function selectRow(){
-        let index = $(this).index()
-        //alert(index)
+        del()
+        let index = $('tr th:first-child').index(this)
         let s = $('tr:nth-child(' + (index+1) + ') td')
         s.addClass('selected')
     }
 
     function del(){
         var sel = document.getElementsByClassName('selected')
-        //alert(sel.length)
         for (var i=sel.length-1; i>=0; --i) {
             sel[i].classList.remove('selected')
         }
