ff6cdc66206f24b27ae83edb3fec385fb596b808
Yanguang-Yang <yyg522743064@gmail.com>
2022-04-08 23:06:15 +0800

    lab7_2

--- lab07/index.js
+++ lab07/index.js
@@ -7,10 +7,22 @@ $(document).ready(function(){
         datatype: 'text',
         success: function(response){
             let rows = response.split('\n')
-            for (i=0; i<rows.length; i++){
+            let tr = $('<tr>')
+                let data = rows[0].split(',')
+                for (j=0; j<data.length; j++){
+                    let th = $('<th>')
+                    th.text(data[j])
+                    tr.append(th)
+                }
+                table.append(tr)
+                
+            for (i=1; i<rows.length; i++){
                 let tr = $('<tr>')
                 let data = rows[i].split(',')
-                for (j=0; j<data.length; j++){
+                let th = $('<th>')
+                th.text(data[0])
+                tr.append(th)
+                for (j=1; j<data.length; j++){
                     let td = $('<td>')
                     td.text(data[j])
                     tr.append(td)
